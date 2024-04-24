86781f4eed834eadbbced82d0fd4c95241236231
laanwj <126646+laanwj@users.noreply.github.com>
2024-04-24 13:58:06 +0200

    wayland: Add needed wayland-* libraries to gen.py

--- scripts/gen.py
+++ scripts/gen.py
@@ -68,6 +68,11 @@ ALL_LIBS = [
 ### fontconfig
 # from src/platformsupport/fontdatabases/fontconfig
 ('fontconfig',      'libfontconfig.so.1',          ['fontconfig/fontconfig.h', 'fontconfig/fcfreetype.h'],  ['freetype']),
+
+### wayland
+('wayland_client',  'libwayland-client.so.0',      ['wayland-client.h', 'wayland-client-core.h', 'wayland-client-protocol.h'], []),
+('wayland_cursor',  'libwayland-cursor.so.0',      ['wayland-cursor.h'],                                                       ['wayland_client']),
+('wayland_egl',     'libwayland-egl.so.1',         ['wayland-egl.h', 'wayland-egl-core.h'],                                    ['wayland_client']),
 ]
 
 INC_DIR = 'include'
